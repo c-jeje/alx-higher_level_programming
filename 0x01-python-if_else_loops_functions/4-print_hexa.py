8293ca03e323cb8941dae6aeb7b6610f6de77eaf
c-jeje <charltonijeje@ymail.com>
2023-11-28 12:36:30 -0800

    white spaces correction

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-# Author - Godswill Kalu
+# Author - Charlton Jeje
 
 """Print numbers 0 to 98 in decimal and hexadecimal."""
 for number in range(0, 99):
-    print("{} = {}".format(number, hex(number)))
+	print("{} = {}".format(number, hex(number)))
