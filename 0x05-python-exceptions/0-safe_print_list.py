f0d4ec9eb3206e0eb31d70248556be0cdd054dba
c-jeje <charltonijeje@ymail.com>
2023-12-18 03:10:55 -0800

    fixed pycodestyle

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    """prints x elements of a list"""
+    """returns real number of elements printed"""
+
+    # if we should print nothing
+    if x == 0:
+        print()
+        return 0
+
+    # track elements printed
+    elements_printed = 0
+
+    # iterate through list and print elements
+    for element in my_list:
+        try:
+            print(f'{element}', end='')
+            elements_printed += 1
+        except exception as e:
+            print(e)
+        if elements_printed == x:
+            print()
+            return elements_printed
+    print()
+    return elements_printed
---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    """prints an integer with "{:d}".format()"""
+    """returns true if integer and false if not"""
+
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as e:
+        return False
