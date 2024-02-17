1f99b0722249089ee85119f0f563da8e166a60ae
c-jeje <charltonijeje@ymail.com>
2024-02-17 08:06:16 -0800

    15-groups.sql

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,4 @@
+-- a script that computes the score average of all records in the table second_table
+-- The database name will be passed as an argument of the mysql command
+
+SELECT AVG(score) AS average FROM second_table;
