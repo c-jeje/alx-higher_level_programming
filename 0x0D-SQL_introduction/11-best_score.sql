ef86a7ca4a414306ccfdd7f3af93e1a25ef84d40
c-jeje <charltonijeje@ymail.com>
2024-02-17 08:02:23 -0800

    11-best_score.sql

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,6 @@
+-- a script that lists all records with a score >= 10 in the table second_table of the database
+-- The database name will be passed as an argument of the mysql command
+
+SELECT score, name FROM second_table
+WHERE score >= 10
+ORDER BY score DESC;
